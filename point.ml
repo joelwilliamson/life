33c1ac5cd8732206a337eaed1a1ec00c76085683
Joel Williamson <joel.s.williamson@gmail.com>
2014-08-10 20:23:31 -0400

    A module that represents points on an integer grid

---
+++ point.ml
@@ -0,0 +1,16 @@
+type t = (int * int)
+let create (x,y) = (x,y)
+
+let x (x,y) = x
+
+let y (x,y) = y
+
+let add (x1,y1) (x2,y2) = (x1+x2),(y1+y2)
+
+let sub (x1,y1) (x2,y2) = (x1-x2),(y1-y2)
+
+let addsub (x1,y1) (x2,y2) = (x1+x2),(y1-y2)
+
+let subadd (x1,y1) (x2,y2) = (x1-x2),(y1+y2)
+
+let string_of_point (x,y) = "("^(string_of_int x)^","^(string_of_int y)^")"
---
+++ point.mli
@@ -0,0 +1,25 @@
+type t = int * int
+
+(* Construct a point from a pair *)
+val create : (int * int) -> t
+
+(* Get the x component of a point *)
+val x : t -> int
+
+(* Get the y component of a point *)
+val y : t -> int
+
+(* Add two points *)
+val add : t -> t -> t
+
+(* Subtract two points *)
+val sub : t -> t -> t
+
+(* Add the x component, subtract the y *)
+val addsub : t -> t -> t
+
+(* Subtract the x component, add the y *)
+val subadd : t -> t -> t
+
+(** Convert the point to a string *)
+val string_of_point : t -> string
