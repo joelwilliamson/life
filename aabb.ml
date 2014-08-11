2bea03f943905ce0fe4ea0d3bbb7190e3b723fb0
Joel Williamson <joel.s.williamson@gmail.com>
2014-08-10 22:22:22 -0400

    A type representing axis-aligned bounding boxes

---
+++ aabb.ml
@@ -0,0 +1,56 @@
+open Core.Std
+
+type t = {
+	center : Point.t ;
+	half_dimension : Point.t ;
+	}
+
+let create center half_dimension =
+	let rec is_power_2 = function
+		| 0 -> false
+		| 1 -> true
+		| n -> (n%2 = 0) && is_power_2 (n/2) in
+	if (Point.x half_dimension < 0) || (Point.y half_dimension < 0)
+	then failwith "Negative dimension not permitted."
+	else if not ((Point.x half_dimension |> is_power_2) && (Point.y half_dimension |> is_power_2))
+	then failwith "Dimensions must be power of two"
+	else {center; half_dimension}
+
+let contains aabb (x,y) =
+	(Point.x aabb.center - Point.x aabb.half_dimension) <= x
+	&& x < (Point.x aabb.center + Point.x aabb.half_dimension)
+	&& (Point.y aabb.center - Point.y aabb.half_dimension) <= y
+	&& y < (Point.y aabb.center + Point.y aabb.half_dimension)
+	
+let intersect a b =
+	let x_d = (Point.x a.center) - (Point.x b.center) |> abs
+	and y_d = (Point.y a.center) - (Point.y b.center) |> abs
+	and max_x = (Point.x a.half_dimension) + (Point.x b.half_dimension)
+	and max_y = (Point.y a.half_dimension) + (Point.y b.half_dimension)
+	in (x_d <= max_x) && (y_d <= max_y)
+	
+let dir_to_point aabb (x,y) =
+	let north = y > (Point.y aabb.center)
+	and east  = x > (Point.x aabb.center)
+	in Direction.from_ne (north,east)
+
+let quadruple orig d =
+	let half_dimension = (2 * (Point.x orig.half_dimension)), (2 * (Point.y orig.half_dimension))
+	and north,east = Direction.to_ne d in
+	let c_x = if east
+		then (Point.x orig.center) + (Point.x orig.half_dimension)
+		else (Point.x orig.center) - (Point.x orig.half_dimension)
+	and c_y = if north
+		then (Point.y orig.center) + (Point.y orig.half_dimension)
+		else (Point.y orig.center) - (Point.y orig.half_dimension)
+	in let center = (c_x,c_y)
+	in {half_dimension; center}
+
+let quadrants bb =
+	let half_dimension = (Point.x bb.half_dimension)/2 , (Point.y bb.half_dimension)/2 in
+	let nw = (Point.x bb.center) - (Point.x half_dimension), (Point.y bb.center) + (Point.y half_dimension)
+	and ne = (Point.x bb.center) + (Point.x half_dimension), (Point.y bb.center) + (Point.y half_dimension)
+	and se = (Point.x bb.center) + (Point.x half_dimension), (Point.y bb.center) - (Point.y half_dimension)
+	and sw = (Point.x bb.center) - (Point.x half_dimension), (Point.y bb.center) - (Point.y half_dimension)
+	in ({center=nw;half_dimension},{center=ne;half_dimension},
+	{center=se;half_dimension},{center=sw;half_dimension})
---
+++ aabb.mli
@@ -0,0 +1,26 @@
+(** An axis-aligned bounding box
+  * This is a pair of half-open intervals, both centered on a given point,
+  * but with possibly different lengths *)
+type t = {
+	center : Point.t ;
+	half_dimension : Point.t
+	}
+
+(** Construct an AABB *)
+val create : Point.t -> Point.t -> t
+
+(** Check if the given point is in the AABB *)
+val contains : t -> Point.t -> bool
+
+(** Check if the two boxes intersect *)
+val intersect : t -> t -> bool
+
+(** Give the direction from the center of the box to the point *)
+val dir_to_point : t -> Point.t -> Direction.t
+
+(** Return a box twice as large in each axis, with the center shifted in the
+  * given direction *)
+val quadruple : t -> Direction.t -> t
+
+(** Return a tuple of the quarters of this box, ordered NW,NE,SE,SW *)
+val quadrants : t -> (t * t * t * t)
