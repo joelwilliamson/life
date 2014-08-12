352200fe8f4ff4b7d5f6e7a2369b647b50b04c50
Joel Williamson <joel.s.williamson@gmail.com>
2014-08-12 19:57:50 -0400

    Modified the example tree

--- quadtree_test.ml
+++ quadtree_test.ml
@@ -1,13 +1,14 @@
 open OUnit2
 
-let qt = Quadtree.(empty (Aabb.create (3,5) (2,8))
+let qt = Quadtree.(empty (Aabb.create (3,5) (8,8))
 	|> insert ~p:(2,6)
 	|> insert ~p:(4,6)
 	|> insert ~p:(4,0)
 	|> insert ~p:(2,4)
 	|> insert ~p:(23,7)
 	|> insert ~p:(-6,2)
-	|> insert ~p:(0,0))
+	|> insert ~p:(0,0)
+	)
 
 let single = Quadtree.singleton (Aabb.create (0,0) (1,1)) (3,5)
 
