75e155829ec2b181e5b8bc47fd4481f3648fe0c4
Joel Williamson <joel.s.williamson@gmail.com>
2014-08-12 19:54:41 -0400

    Added tests for zero-width intervals and quadrants

--- aabb_test.ml
+++ aabb_test.ml
@@ -3,8 +3,10 @@ open OUnit2
 let b1 = Aabb.create (2,2) (2,4)
 and b2 = Aabb.create (3,5) (2,8)
 and b3 = Aabb.create (-3,-3) (1,1)
+and zero_h = Aabb.create (0,0) (1,0)
+and zero_v = Aabb.create (0,0) (0,1)
 
-let contains_1 _ = assert_bool "b1 contains origin" (Aabb.contains b1 (0,0))
+let contains_1 _ = assert_bool "b1 contains origin" (Aabb.contains b1 (1,0))
 let contains_2 _ = assert_bool "b2 doesn't contain origin" (Aabb.contains b2 (0,0)|> not)
 let intersect_1 _ = assert_bool "b1 & b2 intersect" (Aabb.intersect b1 b2)
 let intersect_2 _ = assert_bool "b1 & b3 don't intersect" (Aabb.intersect b1 b3 |> not)
@@ -19,6 +21,18 @@ let quadrants_1 _ = assert_equal ~msg:"quadrants" (Aabb.quadrants b1)
 	(Aabb.create (3,4) (1,2)),
 	(Aabb.create (3,0) (1,2)),
 	(Aabb.create (1,0) (1,2)))
+let quadrant_dir _ = assert_bool "The quadrant in the direction of p contains p"
+	(Aabb.contains
+		(let nw,ne,se,sw = Aabb.quadrants b1 in
+		match Aabb.dir_to_point b1 (2,3) with
+		| NW -> nw
+		| NE -> ne
+		| SE -> se
+		| SW -> sw) (2,3))
+let zero_length_interval_1 _ = assert_bool "Horizontal interval contains origin"
+	((Aabb.contains zero_h (0,0)) && (Aabb.contains zero_h (-1,0)))
+let zero_length_interval_2 _ = assert_bool "Vertical interval contains origin"
+	((Aabb.contains zero_v (0,0)) && (Aabb.contains zero_v (0,-1)))
 
 
 let () =
@@ -34,4 +48,7 @@ let () =
 		;"quad_1">::quad_1
 		;"quad_2">::quad_2
 		;"quadrants">::quadrants_1
+		;"quadrants2">::quadrant_dir
+		;"zero_size1">::zero_length_interval_1
+		;"zero_size2">::zero_length_interval_2
 		])
